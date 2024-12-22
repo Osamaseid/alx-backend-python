2b8f9e0b4c7e3420e211dd149c52be6cb0634c4b
osama seid <osaseid1@gmail.com>
2024-12-22 14:56:45 +0300

    Initial commit - setup Django project with DRF

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,25 +1,3 @@
-import uuid
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Group, Permission
 
-class User(AbstractUser):
-    USER_ROLES = [
-        ('guest', 'Guest'),
-        ('host', 'Host'),
-        ('admin', 'Admin'),
-    ]
-    
-    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    first_name = models.CharField(max_length=30, null=False)
-    last_name = models.CharField(max_length=30, null=False)
-    email = models.EmailField(unique=True, null=False)
-    password_hash = models.CharField(max_length=128, null=False)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    role = models.CharField(max_length=10, choices=USER_ROLES, null=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    groups = models.ManyToManyField(Group, related_name='custom_user_set', blank=True)
-    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_set', blank=True)
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.email})"
+# Create your models here.
